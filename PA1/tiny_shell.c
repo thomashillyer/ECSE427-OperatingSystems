54fce1f4d48443864d2289251e1e050bca442ef6
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2018-10-06 22:55:09 -0400

    Add fork and vfork implementations

--- PA1/tiny_shell.c
+++ PA1/tiny_shell.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,29 +7,66 @@
 #include <unistd.h> //for fork and vfork
 #include <sys/wait.h> //for waitpid
 
+int recentPID = 0;
+
+void intHandler(int SIG) {
+	if (recentPID == 0) {
+		//ignore this call and continue normally
+		exit(0);
+	} else {
+		//kill the top process
+		kill(recentPID, SIGKILL);
+		recentPID = 0;
+	}
+}
+
+void parseCommand(char* line, char** args) {
+	char* deliminators = " \n\t";
+	char* token;
+	int tokenIndex = 0;
+	//get the first token from the string
+	token = strtok(line, deliminators);
+	//while not at the end of the line
+	while (token != NULL) {
+		//store the token in the array
+		args[tokenIndex++] = token;
+		//man page says to pass NULL when the same string should be compared
+		token = strtok(NULL, deliminators);
+	}
+	//make sure to null terminate
+	args[tokenIndex] = '\0';
+	//dont need to return args because it was passed by reference
+}
+
 int clone_function(void* arg);
 
-/*my_system is a functions that create a child process and 
+/*my_system is a functions that create a child process and
 runs the command you passed as the argument*/
-int my_systemF(char* line) {
+int my_systemF(char** args) {
 	pid_t pid; 	//this is the child's PID in the parent process (or -1 if fail and no child created)
-				//this is 0 in the child process
+	//this is 0 in the child process
 	int status;
 
-	printf("Fork line: %s\n", line);
+	//printf("Fork line: %s\n", *args);
 
-	if((pid = fork()) == -1){ //maybe less than 0
-		printf("%s\n", "Failed to create child");
+	if ((pid = fork()) == -1) { //maybe less than 0
+		perror("ERROR: Failed to fork child");
 		exit(1);
-	} else if(pid == 0){
+	} else if (pid == 0) {
 		// you are in the child
-		//do the thing
-
-		//implementation used in system()
-		if(execl("/bin/sh", "sh", "-c", &line, (char*) 0)<0){
-			printf("%s\n", "ERROR: failed to execute command");
-			exit(1);
+		//store the pid so it can be killed with ctrl c
+		recentPID = getpid();
+		if (execvp(args[0], args) < 0) {
+			perror("ERROR: failure in execvp");
 		}
+
+		// old way before i parsed arguments
+		// implementation used in system()
+		// pretty sure this makes another shell inside the child
+		// if(execl("/bin/sh", "sh", "-c", &line, (char*) 0)<0){
+		// 	printf("%s\n", "ERROR: failed to execute command");
+		// 	exit(1);
+		// }
 
 	} else {
 		// you're in parent
@@ -41,19 +80,95 @@ int my_systemF(char* line) {
 	return 0;
 }
 
-int my_system(char* line){
-	#ifdef FORK
-		my_systemF(line);
-	#elif VFORK
-		my_systemV(line);
-	#elif CLONE
-		my_systemC(line); //something
-	#elif PIPE
-		//something
-	#else
-		printf("Regular System call:\n");
-		system(line);
-	#endif
+/*my_system is a functions that create a child process and
+runs the command you passed as the argument*/
+int my_systemV(char** args) {
+	pid_t pid; 	//this is the child's PID in the parent process (or -1 if fail and no child created)
+	//this is 0 in the child process
+	int status;
+	struct timespec start, finish;
+	char* envp[] = {"key=value", '\0'};
+
+	//prepend "/bin/" so that commands can be found
+	char* base;
+	char* newCmd;
+	base = "/bin/\0";
+	int newLen = strlen(base) + strlen(*args) + 1;
+	newCmd = malloc (newLen * sizeof(char));
+	// printf("%s\n", base);
+	// printf("%s\n", *args);
+	strcat(newCmd, base);
+	strcat(newCmd, *args);
+	// printf("%s\n", newCmd);
+	*args = newCmd;
+
+
+	clock_gettime(CLOCK_REALTIME, &start);
+	if ((pid = vfork()) == -1) {
+		perror("ERROR: Failed to fork child");
+		exit(1);
+	} else if (pid == 0) {
+		// you are in the child
+		//store the pid so it can be killed with ctrl c
+		recentPID = getpid();
+		if (execve(args[0], args, envp) < 0) {
+			perror("ERROR: failure in execve");
+		}
+	} else {
+		// you're in parent
+		// you dont need to wait for child
+		// if there is no wait here then the parent returns and will print the prompt before the command has printed
+		waitpid(pid, &status, 0);
+
+		clock_gettime(CLOCK_REALTIME, &finish);
+		long ns = finish.tv_nsec - start.tv_nsec;
+		if (start.tv_nsec > finish.tv_nsec)
+		{	// clock underflow
+			ns += 1000000000;
+		}
+		printf("nanoseconds: %ld\n", ns);
+		return 0;
+	}
+	return 1;
+}
+
+int my_system(char* line) {
+	signal(SIGINT, intHandler);
+	char* args[512];
+	//parse
+	parseCommand(line, args);
+	//hijack process to exit on exit and change directory on cd
+	//since these are not executable commands in the regular sense
+	if (strcmp(*args, "exit") == 0) {
+		exit(0);
+	}
+	// TODO: this is the wrong way to do this (works but doesnt have the expected behaviour with clone)
+	else if (strcmp(*args, "cd") == 0) {
+		//allow soft fails through return statements instead of exit
+		if (args[1] == NULL) {
+			printf("%s\n", "ERROR: missing cd arg");
+			return 1;
+		} else {
+			if (chdir(args[1]) != 0) {
+				perror("ERROR: could not change directory");
+				return 1;
+			}
+			//cd is the command, dont attempt to exec after
+			return 0;
+		}
+	}
+#ifdef FORK
+	my_systemF(args);
+#elif VFORK
+	my_systemV(args);
+#elif CLONE
+	my_systemC(line); //something
+#elif PIPE
+	//something
+#else
+	printf("Regular System call:\n");
+	system(line);
+#endif
 }
 
 int main() {
@@ -62,21 +177,22 @@ int main() {
 	// size_t that will be updated to the buffer size
 	size_t n;
 
-	while(1) {
-		printf("%s","tshell$ ");
+	while (1) {
+		printf("%s", "tshell$ ");
 		//line = get_a_line();
 		//In the event of an error, errno is set to indicate the cause.
-		if(getline(&line, &n, stdin) == -1) {
+		if (getline(&line, &n, stdin) == -1) {
 			// printf("%s\n", "No line");
 			//exit or print error
 			perror("ERROR: ");// not sure what to do here
 			exit(0); //should probably be exit(1)
 		}
 		if (strlen(line) > sizeof(char)) {
-			my_system(line);	
-		} else {
-			exit(0);
+			my_system(line);
 		}
+		//else {
+		//	exit(0);
+		//}
 	}
 }
 
